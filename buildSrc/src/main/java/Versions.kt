6cb5400b5af9c4d58c2e1d4d1a0d63d09869cbcb
Nino <anthony@delcey.fr>
2023-01-10 12:43:07 +0100

    Update Android API to 33, Kotlin to 1.8.0, AGP to 7.3.1

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,13 +2,13 @@ object Versions {
     const val VERSION_NAME = "1.0"
     const val VERSION_CODE = 1
 
-    const val COMPILE_SDK = 32
-    const val TARGET_SDK = 32
+    const val COMPILE_SDK = 33
+    const val TARGET_SDK = 33
     const val MIN_SDK = 21
 
-    const val ANDROID_GRADLE_PLUGIN = "7.2.2"
-    const val KOTLIN = "1.7.10"
-    const val HILT = "2.42"
+    const val ANDROID_GRADLE_PLUGIN = "7.3.1"
+    const val KOTLIN = "1.8.0"
+    const val HILT = "2.44.2"
     const val ROOM = "2.4.3"
     const val KOVER = "0.6.0"
 }
