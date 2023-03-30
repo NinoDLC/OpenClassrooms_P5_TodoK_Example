33114a1e7529286b1144c0eae6885c4db7b4e653
Nino <anthony@delcey.fr>
2023-03-30 17:46:32 +0200

    Upgrade AGP to 7.4.2, Gradle to 7.5

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val TARGET_SDK = 33
     const val MIN_SDK = 21
 
-    const val ANDROID_GRADLE_PLUGIN = "7.3.1"
+    const val ANDROID_GRADLE_PLUGIN = "7.4.2"
     const val KOTLIN = "1.8.10"
     const val HILT = "2.44.2"
     const val ROOM = "2.4.3"
